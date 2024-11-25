dacc30f7175bea26f2aa4807a9f043c698ee79d0
shikou3d <shikou3d@gmail.com>
2024-11-25 12:35:02 +0800

    Ability Damage

--- Source/Aura/Public/AbilitySystem/Abilities/AuraGameplayAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraGameplayAbility.h
@@ -17,4 +17,7 @@ class AURA_API UAuraGameplayAbility : public UGameplayAbility
 public:
 	UPROPERTY(EditDefaultsOnly, Category = "Input")
 	FGameplayTag StartUpInputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage")
+	FScalableFloat Damage;
 };
