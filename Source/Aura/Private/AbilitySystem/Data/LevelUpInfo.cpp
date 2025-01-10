5f26c0bd78b8c24385e11a6be5877d8d11a1fffb
shikou3d <shikou3d@gmail.com>
2025-01-10 18:01:59 +0800

    Level Up Info Data Asset

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,24 @@
+﻿// Copyright Sihao Zhang
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		if (LevelUpInformation.Num() - 1 <= Level) return Level;
+
+		if (XP >= LevelUpInformation[Level].LevelUpRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+	return Level;
+}
