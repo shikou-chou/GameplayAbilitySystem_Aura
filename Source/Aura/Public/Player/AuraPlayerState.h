c05fad0be8eca2e96905a4fcee72959a62c52a81
shikou3d <shikou3d@gmail.com>
2025-01-18 14:58:57 +0800

    Showing Attribute Points in the HUD

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -63,10 +63,10 @@ private:
 	int32 XP = 1;
 
 	UPROPERTY(VisibleAnywhere, ReplicatedUsing=OnRep_AttributePoints)
-	int32 AttributePoints =  1;
+	int32 AttributePoints = 0;
 
 	UPROPERTY(VisibleAnywhere, ReplicatedUsing=OnRep_SpellPoints)
-	int32 SpellPoints =  1;
+	int32 SpellPoints = 0;
 
 	UFUNCTION()
 	void OnRep_Level(int32 OldLevel);
