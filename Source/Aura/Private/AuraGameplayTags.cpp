76a2ab88928ba47f25cf0ca4927b45c2eb1e3502
shikou3d <shikou3d@gmail.com>
2025-01-20 15:51:07 +0800

    Ability Level Requirement

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -128,6 +128,9 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	GameplayTags.Abilities_Fire_FireBolt = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Abilities.Fire.FireBolt"),
 			FString("FireBolt Ability Tag"));
 
+	GameplayTags.Abilities_Lightning_Electrocute = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Abilities.Lightning.Electrocute"),
+			FString("Electrocute Ability Tag"));
+
 	GameplayTags.Abilities_HitReact = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Abilities.HitReact"),
 			FString("Hit React Ability"));
 
