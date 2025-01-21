5f6134049983be79c47bfe89457677c4138b7e37
shikou3d <shikou3d@gmail.com>
2025-01-21 15:22:18 +0800

    Spell Menu Buttons

--- Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -4,8 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "AuraWidgetController.h"
+#include "GameplayTagContainer.h"
 #include "SpellMenuWidgetController.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FSpellGlobeSelectedSignature, bool, bSpendPointsButtonEnabled, bool, bEquipButtonEnabled);
+
+struct FGameplayTag;
 /**
  * 
  */
@@ -20,4 +24,13 @@ public:
 
 	UPROPERTY(BlueprintAssignable)
 	FOnPlayerStatChangedSignature SpellPointsChanged;
+
+	UPROPERTY(BlueprintAssignable)
+	FSpellGlobeSelectedSignature SpellGlobeSelectedDelegate;
+	
+	UFUNCTION(BlueprintCallable)
+	void SpellGlobeSelected(const FGameplayTag& AbilityTag);
+
+private:
+	static void ShouldEnableButtons(const FGameplayTag& AbilityStatus, int32 SpellPoints, bool& bShouldEnableSpellPointsButton, bool& bShouldEnableEquipButton);
 };
